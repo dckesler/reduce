ecb8fce4f04844b578c5e5a11a43d3a0574172d0
Daniel Kesler <danielckesler@gmail.com>
2019-11-03 16:14:00 -0700

    Update problem-three.js

--- problem-three.js
+++ problem-three.js
@@ -1,4 +1,4 @@
 function arrayToObject() {
-  //Turn an array into an object like this [key, value, key2, value2]
-  //{key: value, key2, value2}
+  //Turn an array into an object like this [value, value2, value3]
+  //{index: value, index2: value2, index3: value3}
 }
